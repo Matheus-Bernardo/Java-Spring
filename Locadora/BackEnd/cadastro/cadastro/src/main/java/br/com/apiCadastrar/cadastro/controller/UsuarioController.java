bb3c68dbc7910c42dd4c97331393315f06e84492
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2023-12-19 11:44:41 -0300

    end points crud

--- Locadora/BackEnd/cadastro/cadastro/src/main/java/br/com/apiCadastrar/cadastro/controller/UsuarioController.java
+++ Locadora/BackEnd/cadastro/cadastro/src/main/java/br/com/apiCadastrar/cadastro/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -19,10 +20,22 @@ public class UsuarioController{
 
         return (List<Usuario>) dao.findAll();
     }
-
     @PostMapping
     public Usuario criarUsuario(@RequestBody Usuario usuario){
         Usuario novoUsuario = dao.save(usuario);
         return novoUsuario;
     }
+    @PutMapping
+    public Usuario editarUsuario(@RequestBody Usuario usuario){
+        Usuario novoUsuario = dao.save(usuario);
+        return novoUsuario;
+    }
+
+    @DeleteMapping("/{id}")
+    public Optional<Usuario>  excluirUsuario(@PathVariable Integer id){
+        Optional<Usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
+    }
+
 }
