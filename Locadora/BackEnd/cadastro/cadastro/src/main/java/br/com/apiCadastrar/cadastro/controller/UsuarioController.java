c0b8b186da8a8abe7985311235c808c2558bf1ba
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2023-12-19 01:51:04 -0300

    EndPoint metodo post

--- Locadora/BackEnd/cadastro/cadastro/src/main/java/br/com/apiCadastrar/cadastro/controller/UsuarioController.java
+++ Locadora/BackEnd/cadastro/cadastro/src/main/java/br/com/apiCadastrar/cadastro/controller/UsuarioController.java
@@ -3,20 +3,26 @@ package br.com.apiCadastrar.cadastro.controller;
 import br.com.apiCadastrar.cadastro.DAO.IUsuario;
 import br.com.apiCadastrar.cadastro.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController{
 
     @Autowired // injeção automática
     private IUsuario dao;
 
-    @GetMapping("/usuarios")
+    @GetMapping
     public List<Usuario> listaUsuarios(){
 
         return (List<Usuario>) dao.findAll();
+    }
+
+    @PostMapping
+    public Usuario criarUsuario(@RequestBody Usuario usuario){
+        Usuario novoUsuario = dao.save(usuario);
+        return novoUsuario;
     }
 }
