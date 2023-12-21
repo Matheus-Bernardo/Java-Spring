21bad07c49fcbeb0c83b021dae38847247a33343
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2023-12-21 17:02:56 -0300

    extends JPA

--- Locadora/BackEnd/cadastro/cadastro/src/main/java/br/com/apiCadastrar/cadastro/DAO/IUsuario.java
+++ Locadora/BackEnd/cadastro/cadastro/src/main/java/br/com/apiCadastrar/cadastro/DAO/IUsuario.java
@@ -1,7 +1,8 @@
 package br.com.apiCadastrar.cadastro.DAO;
 
 import br.com.apiCadastrar.cadastro.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface IUsuario extends CrudRepository<Usuario,Integer> {
+public interface IUsuario extends JpaRepository<Usuario,Integer> {
 }
